70c36d7abd07158404b46223e942201ee675d379
claudeny.avelino <claudeny.avelino@hotmail.com>
2025-03-01 23:26:36 -0300

    Criando métodos que modificam valores

--- src/Filme.java
+++ src/Filme.java
@@ -1,26 +1,59 @@
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
-   private int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
 
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
     }
 
     public double getSomaDasAvaliacoes() {
         return somaDasAvaliacoes;
     }
 
+    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
+        this.somaDasAvaliacoes = somaDasAvaliacoes;
+    }
+
     public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
     }
 
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
     void exibeFichaTecnica() {
-        System.out.println("Nome do filme:" + nome);
-        System.out.println("Ano de lançamento:" + anoDeLancamento);
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
     void avalia(double nota) {
