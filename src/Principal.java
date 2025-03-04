d6b02b17e00881f9fbca2e75cc04d9e7ff5cd011
claudeny.avelino <claudeny.avelino@hotmail.com>
2025-03-04 08:41:17 -0300

    Evitando código duplicado.

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,27 @@
+import screematch.modelos.CalculadoraDeTempo;
 import screematch.modelos.Filme;
 import screematch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.pegaMedia());
+
+
         Filme favorito = new Filme();
 
         favorito.setNome("The Matrix");
@@ -27,6 +46,16 @@ public class Principal {
 
         System.out.println("Duração maratonar "+ lost.getNome()+" : " + lost.getDuracaoEmMinutos());
 
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
     }
 
 }
