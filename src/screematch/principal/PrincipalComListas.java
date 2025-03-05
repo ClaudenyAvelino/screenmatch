54af1d95bd05e2a6bcd7446bf19b730531b87a55
claudeny.avelino <claudeny.avelino@hotmail.com>
2025-03-05 17:54:17 -0300

    Definindo outros critérios de ordenação.

--- src/screematch/principal/PrincipalComListas.java
+++ src/screematch/principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import screematch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -40,6 +41,9 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtista);
         System.out.println("Lista de titulos ordenados");
         Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
         System.out.println(lista);
     }
 }
